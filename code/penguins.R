b7ce7991376407c92bb91f3d3d82698aae957dfb
Rania Habib <rhabib@syr.edu>
2025-01-23 11:56:17 -0500

    New project; add combined data

---
+++ code/penguins.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+chinstrap_data <- read_csv("data/chinstrap_data.csv")
+
+adelie_data <- read_csv("data/adelie_data.csv")
+penguin_data <- bind_rows(adelie_data, chinstrap_data)
+
+gentoo_data <- read_csv("data/gentoo_data.csv")
+penguin_data <- bind_rows(gentoo_data, adelie_data, chinstrap_data)
+
+table(penguin_data$Species)
+
+write_csv(penguin_data, "data/penguin_data.csv")
